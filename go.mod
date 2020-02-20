34ac120c77347b0a2ac51cbfac15a9184af407bc
PrisM <thibault.fouache@gmail.com>
2020-02-20 22:11:08 +0100

    Use go-chi.

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module GoVueFront
 
 go 1.13
 
-require github.com/rakyll/statik v0.1.6
+require (
+	github.com/go-chi/chi v4.0.3+incompatible
+	github.com/rakyll/statik v0.1.6
+	github.com/unrolled/render v1.0.2
+)
--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 
 import (
 	_ "GoVueFront/statik"
+	"github.com/go-chi/chi"
+	"github.com/unrolled/render"
 	"log"
 	"net/http"
 
 	"github.com/rakyll/statik/fs"
 )
+
+type User struct {
+	Name string
+	Age  int8
+}
 
 func main() {
 	sfs, err := fs.New()
@@ -15,6 +22,23 @@ func main() {
 		log.Fatal(err)
 	}
 
-	http.Handle("/", http.StripPrefix("/", http.FileServer(sfs)))
-	_ = http.ListenAndServe("localhost:8080", nil)
+	mux := chi.NewRouter()
+	r := render.New()
+
+	mux.Get("/*", func(writer http.ResponseWriter, request *http.Request) {
+		fileServer := http.StripPrefix("/", http.FileServer(sfs))
+		fileServer.ServeHTTP(writer, request)
+	})
+
+	mux.Get("/api", func(writer http.ResponseWriter, request *http.Request) {
+		_ = r.JSON(writer, http.StatusOK, User{
+			Name: "Joe",
+			Age:  34,
+		})
+	})
+
+	err = http.ListenAndServe("localhost:8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
